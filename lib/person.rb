f4e1535ce60687582358c2f852c238dde988c1df
BartTheScrivner <dframe22@gmail.com>
2020-06-04 13:08:12 -0400

    Working on #initialize and #happiness

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
 # your code goes here
+require 'pry'
+class Person
+  attr_accessor :bank_account, :happiness
+  attr_reader :name
+  
+  def initialize(name, happiness = 8)
+    @name = name
+    @bank_account = 25
+    @happiness = happiness
+  end
+
+
+  def happiness=(num)
+    @happiness = num
+    if @happiness > 10 
+      "No one is that happy"
+      @happiness = 10
+    else @happiness < 0
+      "Cheer up"
+      @happiness = 0
+    end
+  end
+end
